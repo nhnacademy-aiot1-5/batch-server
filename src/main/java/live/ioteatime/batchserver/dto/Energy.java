21b90069bb0041bb08256ceddd15d8c3131e332f
huhsiyoung <july433@naver.com>
2024-05-24 11:29:12 +0900

    fix : 오타 수정

--- src/main/java/live/ioteatime/batchserver/dto/Energy.java
+++ src/main/java/live/ioteatime/batchserver/dto/Energy.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class TypeKwh {
+public class Energy {
 
     private String type;
     private Double kwh;
--- src/main/java/live/ioteatime/batchserver/repository/KwhRepository.java
+++ src/main/java/live/ioteatime/batchserver/repository/KwhRepository.java
@@ -1,6 +1,6 @@
 package live.ioteatime.batchserver.repository;
 
-import live.ioteatime.batchserver.dto.TypeKwh;
+import live.ioteatime.batchserver.dto.Energy;
 
 import java.util.List;
 
@@ -10,10 +10,10 @@ import java.util.List;
 public interface KwhRepository {
 
     /**
-     * 장소(타입)으로 전력량을 조회하는 메소드입니다.
+     * 장소(타입)으로 전력량을 조회하고 Energy 리스트를 메소드입니다.
      *
      * @param type 장소를 나타냅니다.
-     * @return 리스트 형태로 TypeKwh(type, kwh) 객체를 반환합니다.
+     * @return 리스트 형태로 Energy(type, kwh) 객체를 반환합니다.
      */
-    List<TypeKwh> findAllByType(List<String> type);
+    List<Energy> findAllByType(List<String> type);
 }
