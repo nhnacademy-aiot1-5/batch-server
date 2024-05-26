b02f6a11409e835fa37f1082c8b15d522098cfe7
Jeongyong Lee <hua9378@gmail.com>
2024-05-26 23:35:00 +0900

    feat: 월별 전기 요금 레포지토리 구현

---
+++ src/main/java/live/ioteatime/batchserver/repository/impl/MonthlyConsumptionRepositoryImpl.java
@@ -0,0 +1,34 @@
+package live.ioteatime.batchserver.repository.impl;
+
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import live.ioteatime.batchserver.domain.Consumption;
+import live.ioteatime.batchserver.repository.MonthlyConsumptionRepository;
+import live.ioteatime.batchserver.setter.ConsumptionStatementSetter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MonthlyConsumptionRepositoryImpl implements MonthlyConsumptionRepository {
+
+    private static final String INSERT_SQL = "insert into monthly_electricity_consumption values (?, ?, ?, ?)";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Boolean saveAll(List<Consumption> consumptions) {
+        BatchPreparedStatementSetter setter = new ConsumptionStatementSetter(consumptions);
+        int[] result = jdbcTemplate.batchUpdate(INSERT_SQL, setter);
+
+        return isSuccess(result);
+    }
+
+    private boolean isSuccess(int[] result) {
+        return Arrays.stream(result)
+                     .allMatch(i -> i == Statement.SUCCESS_NO_INFO);
+    }
+}
