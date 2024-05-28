9669abc12f4668dc8d50a10bd33cc15bc0a80371
Jeongyong Lee <hua9378@gmail.com>
2024-05-28 20:37:07 +0900

    test: Kwh 레포지토리 테스트 작성

--- src/test/java/live/ioteatime/batchserver/repository/KwhRepositoryTest.java
+++ src/test/java/live/ioteatime/batchserver/repository/KwhRepositoryTest.java
@@ -1,6 +1,14 @@
 package live.ioteatime.batchserver.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
 import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.QueryApi;
+import com.influxdb.query.FluxRecord;
+import com.influxdb.query.FluxTable;
+import java.util.LinkedHashMap;
 import java.util.List;
 import live.ioteatime.batchserver.domain.Energy;
 import live.ioteatime.batchserver.repository.impl.KwhRepositoryImpl;
@@ -9,6 +17,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 class KwhRepositoryTest {
@@ -16,14 +25,38 @@ class KwhRepositoryTest {
     @Mock
     InfluxDBClient influxDBClient;
 
+    @Mock
+    QueryApi queryApi;
+
     @InjectMocks
     KwhRepositoryImpl kwhRepository;
 
     @Test
     void findAllByType() {
-        kwhRepository.findAllByType(List.of("main", "ac", "ac_indoor_unit"))
-                     .stream()
-                     .map(Energy::getKwh)
-                     .forEach(System.out::println);
+        given(influxDBClient.getQueryApi()).willReturn(queryApi);
+        given(queryApi.query(anyString())).willReturn(List.of(createDummyFluxTable()));
+
+        List<String> types = List.of("main");
+
+        List<Energy> energyList = kwhRepository.findAllByType(types);
+
+        assertThat(energyList).element(0)
+                              .hasFieldOrPropertyWithValue("place", "office")
+                              .hasFieldOrPropertyWithValue("type", "main")
+                              .hasFieldOrPropertyWithValue("kwh", 22.2);
+    }
+
+    private FluxTable createDummyFluxTable() {
+        FluxTable fluxTable = new FluxTable();
+        FluxRecord fluxRecord = new FluxRecord(0);
+        LinkedHashMap<String, Object> values = new LinkedHashMap<>();
+        values.put("place", "office");
+        values.put("type", "main");
+        values.put("_value", 22.2);
+
+        ReflectionTestUtils.setField(fluxRecord, "values", values);
+        ReflectionTestUtils.setField(fluxTable, "records", List.of(fluxRecord));
+
+        return fluxTable;
     }
 }
