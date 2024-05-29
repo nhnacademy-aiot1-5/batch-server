3fa0dac4bc035e2886371e7c9901985200fd7063
huhsiyoung <july433@naver.com>
2024-05-29 21:35:28 +0900

    fix : 기본요금이 잘못 계산되던 오류 수정

--- src/main/java/live/ioteatime/batchserver/util/BillUtils.java
+++ src/main/java/live/ioteatime/batchserver/util/BillUtils.java
@@ -9,6 +9,8 @@ import java.time.LocalDate;
  * {@code BillUtils} 유틸 클래스는 전기요금 계산 시 필요한 요금들을 반환합니다.
  */
 public class BillUtils {
+
+    private static final int CONTRACTED_POWER = 10;
     private static final int CLIMATE_CHANGE_CHARGE = 9;
     private static final int FUEL_COST_ADJUSTMENT_CHARGE = 5;
     private static final double VAT = 0.1;
@@ -19,29 +21,38 @@ public class BillUtils {
     }
 
     /**
-     * 전기요금을 반환하는 메서드입니다.
+     * 청구요금을 반환하는 메서드입니다.
      * @param kwhUsage 전력사용량입니다.
-     * @param day 전력을 사용한 일수입니다.
-     * @return 전기요금을 반환합니다.
+     * @return 청구요금을 반환합니다.
      */
-    public static Long calculateElectricityBill(Double kwhUsage, int day) {
-        long electricityBill = getGeneralCharge(day)
-                + getDemandCharge(kwhUsage)
-                + getClimateChangeCharge(kwhUsage)
-                + getFuelCostAdjustmentCharge(kwhUsage);
+    public static Long getBillingCharge(Double kwhUsage) {
+        long electricityBill = getElectricityBill(kwhUsage);
 
         return electricityBill + getVAT(electricityBill) + getElectricityIndustryInfraFund(electricityBill);
     }
 
     /**
+     * 전기요금을 반환하는 메서드입니다.
+     * @param kwhUsage 전력 사용량입니다.
+     * @return 전기 요금을 반환합니다.
+     */
+    public static Long getElectricityBill(Double kwhUsage) {
+        return getGeneralCharge()
+                + getDemandCharge(kwhUsage)
+                + getClimateChangeCharge(kwhUsage)
+                + getFuelCostAdjustmentCharge(kwhUsage);
+    }
+
+    /**
      * 기본요금을 반환하는 메서드입니다.
-     * @param day 전력을 사용한 일수입니다.
+     * <br>
+     * 계약 전력 10kwh, 일반용 고압A I를 기준으로 계산합니다.
      * @return 사용한 일수만큼 기본요금을 반환합니다.
      */
-    public static Long getGeneralCharge(int day) {
+    public static Long getGeneralCharge() {
         long generalCharge = SupplyVoltage.HIGH_VOLTAGE_A_OPTION_I.getGeneralCharge();
 
-        return generalCharge * day;
+        return generalCharge * CONTRACTED_POWER;
     }
 
     /**
@@ -86,10 +97,12 @@ public class BillUtils {
 
     /**
      * 전력산업발전기금을 반환하는 메서드입니다.
+     * <br>
+     * 10원 미만은 버립니다.
      * @param electricityBill 전기요금입니다.
      * @return 전력산업발전기금을 반환합니다.
      */
     public static Long getElectricityIndustryInfraFund(long electricityBill) {
-        return (long) Math.floor(electricityBill * ELECTRICITY_INDUSTRY_INFRASTRUCTURE_FUND);
+        return ((long) (electricityBill * ELECTRICITY_INDUSTRY_INFRASTRUCTURE_FUND)) / 10 * 10;
     }
 }
